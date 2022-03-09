1ff090a53ceab016e246ddacb1a31c1d98a4955d
vakil-neelin <vakilneelin@gmail.com>
2022-03-09 17:14:30 -0500

    Updated Bunny Hop

--- bunny_hop.py
+++ bunny_hop.py
@@ -1,39 +1,31 @@
-from datetime import datetime
-
-
 def bunnyHops(hop_distance):
     hop_combinations = []
     hop_options = [1, 2, 3]
-    length_list = []
 
     for option in hop_options:
         being_worked_on = []
 
         if hop_distance > option:
-            being_worked_on.append([option, ])
-            start_time = datetime.now()
-            count = 0
+            being_worked_on.append([option])
+
             while being_worked_on:
                 item = being_worked_on.pop()
                 item_sum = sum(item)
 
                 # Loops Through Options And Appends If Completed Or Still Needs To Be Worked On
-                for i in hop_options:
-                    temp_sum = item_sum + i
-                    count += 1
+                for inner_option in hop_options:
+                    temp_sum = item_sum + inner_option
+
+                    # Checks If The Item Is Done Or Needs More Work
                     if temp_sum < hop_distance:
                         temp_item = list(item)
-                        temp_item.append(i)
+                        temp_item.append(inner_option)
                         being_worked_on.append(temp_item)
                     elif temp_sum == hop_distance:
                         temp_item = list(item)
-                        temp_item.append(i)
+                        temp_item.append(inner_option)
                         hop_combinations.append(temp_item)
                         break
-                length_list.append(len(being_worked_on))
-            print(option, count, datetime.now() - start_time)
-            print(max(length_list))
-            print(len(hop_combinations))
 
         # Handles Case When The Distance Equals The Hop Value
         elif option == hop_distance:
@@ -42,21 +34,25 @@ def bunnyHops(hop_distance):
     return hop_combinations
 
 
-number_of_expected = [0, 1, 2, 4]
-for x in range(4, 25):
-    expected_count = number_of_expected[x - 3:x]
-    number_of_expected.append(sum(expected_count))
-
-for x in range(0, 25):
-    start = datetime.now()
-    number_of_hops = x
-    hop_combos = bunnyHops(number_of_hops)
-    print("Number Of Hops", x, number_of_expected[x], (datetime.now() - start).total_seconds())
-    if len(hop_combos) != number_of_expected[x]:
-        print("Number Of Hops", x)
-        print("Expected Number", number_of_expected[x])
-        print("Actual Number", len(hop_combos))
-    for hops in hop_combos:
-        if sum(hops) != number_of_hops:
-            print("Hops", hops)
-            print("Number Of Hops", x)
+# # Testing Code
+# from datetime import datetime
+#
+# number_of_expected = [0, 1, 2, 4]
+# for x in range(4, 25):
+#     expected_count = number_of_expected[x - 3:x]
+#     number_of_expected.append(sum(expected_count))
+#
+# # Records Accuracy And Efficiency
+# for x in range(0, 25):
+#     start = datetime.now()
+#     number_of_hops = x
+#     hop_combos = bunnyHops(number_of_hops)
+#     print("Number Of Hops", x, number_of_expected[x], (datetime.now() - start).total_seconds())
+#     if len(hop_combos) != number_of_expected[x]:
+#         print("Number Of Hops", x)
+#         print("Expected Number", number_of_expected[x])
+#         print("Actual Number", len(hop_combos))
+#     for hops in hop_combos:
+#         if sum(hops) != number_of_hops:
+#             print("Hops", hops)
+#             print("Number Of Hops", x)
